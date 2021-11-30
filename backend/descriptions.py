d2fea6b85294dc3fabe83af9bdf3e2a28fa37cdc
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2021-11-30 19:36:50 +0100

    Text description for hand configurations

--- backend/descriptions.py
+++ backend/descriptions.py
@@ -3,6 +3,9 @@
 #
 # Textual descriptions for SignWriting graphemes.
 
+import re
+
+
 heads = {
     'face': 'Este símbolo representa la *cabeza* o la cara del signante.',
     'nose': 'Este símbolo representa la *nariz*.',
@@ -63,7 +66,7 @@ def get_description(tags):
     elif cl == 'DIAC':
         return diacs.get(sh)
     elif cl == 'HAND':
-        return 'hand'
+        return hand_description(tags['SHAPE'], tags['VAR'], tags['ROT'], tags['REF'])
     elif cl == 'STEM':
         if sh == 's':
             return 'La varilla simple indica que el movimiento es *horizontal*, paralelo al suelo.'
@@ -87,3 +90,94 @@ def get_description(tags):
             return 'La varilla doble indica que el movimiento es *vertical*, paralelo a la pared.'
         else:
             return None
+
+
+fixed_hands = {
+    'picam++': 'El puño cerrado.',
+    'TE': 'La mano como en la "T" del dactilológico.',
+}
+
+hand_regex = re.compile(r'([picamPICAM]+)([rg]?)([+-]?)(O?)')
+
+
+def hand_description(sh, var, rot, ref):
+    if sh in fixed_hands:
+        return fixed_hands[sh]
+
+    r = hand_regex.search(sh)
+    if r is None:
+        return None
+
+    fingers = r.group(1)
+    flex_mode = r.group(2)
+    contact = r.group(3)
+    others = r.group(4) == 'O'
+
+    # Fingers
+    fingers_list = []
+    ext_flag = False
+    if fingers.find('P') >= 0:
+        fingers_list.append('pulgar')
+        ext_flag = True
+    elif fingers.find('p') >= 0:
+        fingers_list.append('pulgar')
+    if fingers.find('I') >= 0:
+        fingers_list.append('índice')
+        ext_flag = True
+    elif fingers.find('i') >= 0:
+        fingers_list.append('índice')
+    if fingers.find('C') >= 0:
+        fingers_list.append('corazón')
+        ext_flag = True
+    elif fingers.find('c') >= 0:
+        fingers_list.append('corazón')
+    if fingers.find('A') >= 0:
+        fingers_list.append('anular')
+        ext_flag = True
+    elif fingers.find('a') >= 0:
+        fingers_list.append('anular')
+    if fingers.find('M') >= 0:
+        fingers_list.append('meñique')
+        ext_flag = True
+    elif fingers.find('m') >= 0:
+        fingers_list.append('meñique')
+
+    if len(fingers_list) == 5:
+        text = 'Los dedos'
+        plural = 's'
+    elif len(fingers_list) > 1:
+        plural = 's'
+        text = 'Los dedos '+', '.join(fingers_list)
+        last_comma = text.rfind(',')
+        rest = text[last_comma+2:]
+        text = text[:last_comma] + (' e ' if rest.startswith('í') else ' y ') + rest
+    else:
+        plural = ''
+        text = f'El dedo {fingers_list[0]}'
+
+    if ext_flag:
+        text += ' extendido'+plural
+    else:
+        if flex_mode != 'r' and flex_mode != 'g':
+            text += f' curvo{plural}'
+            if contact == '-':
+                text += ' y juntos'
+        elif flex_mode == 'r':
+            text += f' recto{plural}, flexionado{plural} por el nudillo'
+        elif flex_mode == 'g':
+            text += f' flexionado{plural} por las falanges, como una garra'
+
+    if contact == '-':
+        text += ' y juntos'
+    elif contact == '+':
+        if len(fingers_list) == 2:
+            text += f', con el pulgar tocando la yema del {fingers_list[1]}'
+        else:
+            text += ', con el pulgar tocando las yemas de los demás'
+
+    if others:
+        text += '. Los demás dedos extendidos.'
+    else:
+        text += '.'
+
+    return text
